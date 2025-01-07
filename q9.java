04776993a3dc3a3074252296b55e5a250686da6a
Souparno Goswami <165120778+Souparno467@users.noreply.github.com>
2025-01-07 12:03:37 +0530

    .

---
+++ q9.java
@@ -0,0 +1,15 @@
+//Author Souparno
+class Question9{
+	  static int factorial(int num){
+		int num2=1;
+		for(int i=1;i<=num;i++){      //Method for Factorial Logic
+			num2*=i;
+		}
+		return num2;
+	}
+	public static void main(String[]args){        //Main method
+		int number=6;
+		int fact=factorial(number);
+		System.out.println("The Factorial of "+number+"is:"+fact);
+	}
+}
